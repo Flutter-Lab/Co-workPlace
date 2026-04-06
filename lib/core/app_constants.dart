77dd354d5901383d1c66028c94ad199e5b0b7436
Abir Hossain <abir2526@gmail.com>
2026-04-06 04:29:57 -0400

    Goal Updated

--- lib/core/app_constants.dart
+++ lib/core/app_constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String appVersion = "1.0.5";
+  static const String appVersion = "1.0.6";
 
   /// Set to true to re-enable peer voting on tasks.
   static const bool votingEnabled = false;
